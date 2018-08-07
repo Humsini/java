eb1f792701d3307dc0562783eba6dbc3d0f4b0e2
Humsini <35861423+Humsini@users.noreply.github.com>
2018-08-07 15:58:01 +0530

    Update 1 st pro.java

--- 1 st pro.java
+++ 1 st pro.java
@@ -5,15 +5,15 @@ public static void main(String args[])
 int i=3;
 if(i==0)
 {
-System.out.println("zero");
+System.out.println("Zero");
 }
 else if(i>0)
 {
-System.out.println("positive");
+System.out.println("Positive");
 }
 else
 {
-System.out.println("negative");
+System.out.println("Negative");
 }
 }
 }
