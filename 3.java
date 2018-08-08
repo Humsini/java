c6965c69d8539909ef9990345982fb57f9a1b256
Humsini <35861423+Humsini@users.noreply.github.com>
2018-08-08 13:23:21 +0530

    Update 3.java

--- 3.java
+++ 3.java
@@ -5,9 +5,9 @@ class main{
   char c=s.next().charAt(0);
   if((c>='a' && c<='z') || (c>='A' && c<='Z')){
   if(c=='a'|| c=='A'|| c=='e'||c=='o'||c=='i'||c=='u'||c=='E'||c=='O'||c=='I'||c=='U')
-   system.out.println("Vowels");
+   system.out.println("Vowel");
   else
-   System.out.println("Consonants");
+   System.out.println("Consonant");
    }
    else
     System.out.println("invalid");
