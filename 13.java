1f507283cc9debd9cfec2442038c938935edaa3d
Humsini <35861423+Humsini@users.noreply.github.com>
2018-08-08 12:54:10 +0530

    Create 13.java

---
+++ 13.java
@@ -0,0 +1,16 @@
+class main{
+ public static void main(String args[]){
+   Scanner s=new Scanner(System.in);
+   int N=s.nextInt();
+   int count=0;
+   if(N<=1000){
+    for(int i=2;i<=n/2;i++){
+      if(n/i==0){
+        count=1;
+    }
+ }
+ if(count==0)
+   System.out.println("yes");
+ else
+   System.out.println("no");
+}
