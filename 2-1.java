4dff04b78fd00d44eadbafc7d485d10bd18c7e88
Humsini <35861423+Humsini@users.noreply.github.com>
2018-08-08 18:47:32 +0530

    Create 2-1.java

---
+++ 2-1.java
@@ -0,0 +1,9 @@
+class main{
+ public static void main(String args[]){
+   Scanner s=new Scanner(System.in);
+   String a=s.nextLine();
+   StringBuilder sb=new StringBuilder(a);
+   System.out.println(sb.reverse());
+   
+ }
+}
