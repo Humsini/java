fd4793a8ade5193d2051a87e9372a600bb08e360
Humsini <35861423+Humsini@users.noreply.github.com>
2018-08-08 19:51:27 +0530

    Update 2-10.java

--- 2-10.java
+++ 2-10.java
@@ -2,8 +2,8 @@ import java.util.*;
 public class MyClass{
  public static void main(String args[]){
    Scanner sc=new Scanner(System.in);
-   String a=sc.nextLine();
-   String b=sc.nextLine();
+   String a=sc.next();
+   String b=sc.next();
    int count=0;
    if(a.length()<=100000 && b.length()<=100000){
    for(int i=0;i<a.length();i++)
