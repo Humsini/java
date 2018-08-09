fb2bf21c27d492e741593da7eab51b62ae4e4ee9
Humsini <35861423+Humsini@users.noreply.github.com>
2018-08-09 20:49:44 +0530

    Create 41.java

---
+++ 41.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Main 
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        String sh=s.next();
+        int a=s.nextInt();
+        for(int i=0;i<a;i++)
+        {
+            System.out.println(sh);
+        }
+    }
+}
